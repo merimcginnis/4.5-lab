807f73a0b4a2afe26dd6eabeff39a9a21fe8f646
merimcginnis <166745413+merimcginnis@users.noreply.github.com>
2025-02-10 01:02:30 +0000

    trying to commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
 };
